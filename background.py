d5edcea9d4bc63b3c2ed511127b71fdbb7ef8016
John Fish <john@johnafish.ca>
2020-06-07 18:49:03 -0400

    Add Background Generation
    `background.py` now contains a class which can return an arbitrarily
    sized, perlin-noise filled image to look like "paper".

---
+++ background.py
@@ -0,0 +1,30 @@
+from PIL import Image
+from noise import pnoise2
+import numpy as np
+
+class Background:
+    def __init__(self, dimensions, octaves):
+        self.dimensions = dimensions
+        self.octaves = octaves
+
+    def image(self):
+        white = Image.new("RGB", self.dimensions, (255, 255, 255))
+        texture = self.texturize()
+        return Image.blend(white, texture, 0.1)
+
+    def texturize(self):
+        texture = np.empty([self.dimensions[0], self.dimensions[1]])
+        freq = 16.0 * self.octaves
+
+        for i in range(self.dimensions[0]):
+            for j in range(self.dimensions[1]):
+                v = int(pnoise2(i / freq, j / freq, self.octaves) * 127.0 + 128.0)
+                texture[i][j] = v 
+        print(texture)
+        return Image.fromarray(texture).convert("RGB") 
+        
+
+if __name__  == "__main__":
+    bg = Background((1000,1000), octaves=6)
+    im = bg.image()
+    im.show()
